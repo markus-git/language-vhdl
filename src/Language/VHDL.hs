eca5c25876c22a8ad04110d715448237217d251b
Markus <mararon@chalmers.se>
2015-08-18 14:40:54 +0200

    bump

---
+++ src/Language/VHDL.hs
@@ -0,0 +1,9 @@
+module Language.VHDL
+  ( module Language.VHDL.Syntax
+  , module Language.VHDL.Pretty
+  ) where
+
+import Language.VHDL.Syntax
+import Language.VHDL.Pretty
+
+--------------------------------------------------------------------------------
