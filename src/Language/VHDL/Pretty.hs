4cd1ab24b977a6bb76f6a6b0afb0bd03d3dcd973
Markus <mararon@chalmers.se>
2015-09-15 11:41:17 +0200

    fixed spelling of architecture

--- src/Language/VHDL/Pretty.hs
+++ src/Language/VHDL/Pretty.hs
@@ -76,7 +76,7 @@ instance Pretty ArchitectureBody where
       header = text "ARCHITECTURE" <+> pp i
            <+> text "OF" <+> pp n
            <+> text "IS"
-      footer = text "END ARCITECTURE" <+> pp i <+> semi
+      footer = text "END ARCHITECTURE" <+> pp i <+> semi
 
 --instance Pretty ArchitectureDeclarativePart where pp = undefined
 
